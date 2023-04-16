9d598e78a13cd9073f26eb1eb0352ba7ffe02b6c
Daniel Whitenack <whitenack.daniel@gmail.com>
2023-04-16 17:10:43 -0400

    add microsoft clarity

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 export default {
   logo: <span>♜ prediction<b>Guard</b></span>,
@@ -16,6 +17,25 @@ export default {
   footer: {
     text: '♜ Prediction Guard docs',
   },
+
+  // Add in static head tags.
+  // See https://nextjs.org/docs/api-reference/next/head
+  head: (
+    <>
+      <Script id="clarityinsert"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", 'gpm8utc3xm');`,
+        }}
+      />
+    </>
+  ),
+
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Prediction Guard'
