91c6efa45c927a3908732c5bc2f35427af3a0c78
edmcquinn <73904683+edmcquinn@users.noreply.github.com>
2023-05-08 11:26:48 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+      {/* Clarity Tracking Tag */}
+<script
+   dangerouslySetInnerHTML={
+                 {
+     __html: `
+         (function(c,l,a,r,i,t,y){
+             c[a] = c[a] || function () { (c[a].q = c[a].q || 
+             []).push(arguments) };
+             t=l.createElement(r);
+             t.async=1;
+             t.src="https://www.clarity.ms/tag/"+i;
+             y=l.getElementsByTagName(r)[0];
+             y.parentNode.insertBefore(t,y);
+         })(window, document, "clarity", "script", "gpm8utc3xm");`,
+   }}
+ />;
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
