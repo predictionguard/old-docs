48e648a2639e7732a4af25152d778efec3904620
edmcquinn <73904683+edmcquinn@users.noreply.github.com>
2023-05-08 11:01:08 -0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -20,21 +20,21 @@ export default {
 
   // Add in static head tags.
   // See https://nextjs.org/docs/api-reference/next/head
-<script
-   dangerouslySetInnerHTML={
-                 {
-     __html: `
-         (function(c,l,a,r,i,t,y){
-             c[a] = c[a] || function () { (c[a].q = c[a].q || 
-             []).push(arguments) };
-             t=l.createElement(r);
-             t.async=1;
-             t.src="https://www.clarity.ms/tag/"+i;
-             y=l.getElementsByTagName(r)[0];
-             y.parentNode.insertBefore(t,y);
-         })(window, document, "clarity", "script", "gpm8utc3xm");`,
-   }}
- />;
+  head: (
+    <>
+      <Script id="clarityinsert"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", 'gpm8utc3xm');`,
+        }}
+      />
+    </>
+  ),
 
   useNextSeoProps() {
     return {
